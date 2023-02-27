86c57005f7dbcac71086dc4c4598f450ad0f6a64
teklu <teklufa@gmail.com>
2023-02-27 16:59:14 +0300

    send number of clients connected to any listeners in real time

--- index.js
+++ index.js
@@ -22,8 +22,19 @@ app.get('/', (req, res) => {
 })
 
 // executes when a new connection is created with the client
+let clients = []
 io.on('connection', (socket) => {
+  // push the client socket in the client array, for collecting stat
+  clients.push(socket)
   console.log('client connected', socket.id);
+  io.emit('new connection', { count: clients.length })
+  socket.on('disconnect', (reason) => {
+    console.log('client disconnected,', reason);
+    // when the client disconnected remove the connection from the clients stat
+    const idx = clients.indexOf(socket)
+    clients.splice(idx, 1)
+    io.emit('client left', { count: clients.length })
+  })
 })
 
 server.listen(3001, () => {
